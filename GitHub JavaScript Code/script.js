6ae6fd72f80d01abe827f229b5ffbb5af9812942
Dewrong <ysidda33@gmail.com>
2022-02-11 11:36:47 -0600

    number, left side, scrollbar

--- GitHub JavaScript Code/script.js
+++ GitHub JavaScript Code/script.js
@@ -1,25 +1,27 @@
 function pokedexEntry() {
-    let information = "replaced";
+    let information = "";
     let speciesName = '';
     let img = '';
+    let pokeTypeString = '';
 
-    fetch('https://pokeapi.co/api/v2/pokemon/torterra')
+    fetch('https://pokeapi.co/api/v2/pokemon/oddish')
         .then(species => species.json())
         .then(data => {
+            const typestring = data.types[0].type.name;
+            pokeTypeString = typestring.charAt(0).toUpperCase() + typestring.slice(1)
+                if(data.types[1]){
+                    let secondType = data.types[1].type.name
+                    pokeTypeString = typestring.charAt(0).toUpperCase() + typestring.slice(1) +"/"+ secondType.charAt(0).toUpperCase()+secondType.slice(1)
+                }
             speciesName = data.name.charAt(0).toUpperCase()+ data.name.slice(1);
             document.getElementById("speciesName").innerHTML = speciesName,
-            document.getElementById("height").innerHTML = ((0.328084 * data.height).toFixed(2)+" feet")
-            document.getElementById("weight").innerHTML = ((0.220462 * data.weight).toFixed(2) +" lbs")
-            document.getElementById("photo").src = data.sprites.front_default             
-            document.getElementById("shinyPhoto").src = data.sprites.front_shiny
+            document.getElementById("speciesNumber").innerHTML = "#"+data.id
+            document.getElementById("speciesType").innerHTML = "Type: " + pokeTypeString
+            document.getElementById("height").innerHTML = ("Height: "+(0.328084 * data.height).toFixed(2)+" ft")
+            document.getElementById("weight").innerHTML = ("Weight: "+(0.220462 * data.weight).toFixed(2) +" lbs")
+            document.getElementById("photo").src = data.sprites.other.home.front_default             
+            document.getElementById("shinyPhoto").src = data.sprites.other.home.front_shiny
         })
-    //speciesName= getPokemonName(){
-    //  return 0;
-    //};
-    //getPokemonHeight();
-    //getPokemonWeight();
 }
-//species => species.json()
-//(function(species){return species.json();})
 
 pokedexEntry();
